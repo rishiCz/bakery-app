6541714f213f675398e00d9b1702eeb364c60083
Rishi Malik <rishimalik97@gmail.com>
2023-08-08 23:38:43 +0530

    Added component to CartItem and ProductCard

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,34 +1,16 @@
-import React, { useState } from "react";
 import styles from "./style.module.css";
 import { useDispatch } from "react-redux";
-import { addToCart } from '../../store/slices/cartSlice';
-import { addGivenQuantity } from '../../store/slices/cartSlice';
-import { decreaseQuantity } from '../../store/slices/cartSlice';
 import { removeItem } from "../../store/slices/cartSlice";
 import { FaTrash } from "react-icons/fa";
+import QuantitySelector from '../QuantitySelector';
 
 const CartItem = (props) => {
   const product = {...props};
   const dispatch = useDispatch();
-
-  const handleQuantityChange = (event) => {
-    const newQuantity = parseInt(event.target.value, 10);
-    if (!isNaN(newQuantity) && newQuantity >= 1) {
-      const newprops = {...props,quantity:newQuantity};
-      console.log(newQuantity);
-      dispatch(addGivenQuantity(newprops));
-    }
-  };
-  const incQuantity = () =>{
-    dispatch(addToCart(props));
-  }
-  const decQuantity = () =>{
-    dispatch(decreaseQuantity(props));
-  }
   const deleteButtonClick = () =>{
+    console.log(props)
     dispatch(removeItem(props));
   }
-
   return (
     <div className={styles.productCard}>
       <img
@@ -39,24 +21,9 @@ const CartItem = (props) => {
       <div className={styles.detailsBackground}>
         <h3 className={styles.productName}>{product.name}</h3>
         <p className={styles.productPrice}>${product.price}</p>
-        <button
-          className={styles.controlButton}
-          onClick={decQuantity}
-        >
-          -
-        </button>
-        <input
-          type="number"
-          value={product.quantity}
-          onChange={handleQuantityChange}
-          className={styles.quantityInput}
-        />
-        <button
-          className={styles.controlButton}
-          onClick={incQuantity}
-        >
-          +
-        </button>
+        <div className={styles.quantitySelectorHolder}>
+          <QuantitySelector product ={product}/>
+        </div>
         <p className={styles.totalPrice}>${(product.quantity * product.price).toFixed(2)}</p>
       </div>
       <button className={styles.deleteButton} onClick={deleteButtonClick}>
--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,27 +1,15 @@
 import styles from './style.module.css';
-import { useDispatch } from "react-redux";
-import { addToCart } from '../../store/slices/cartSlice';
+import QuantitySelector from '../QuantitySelector';
 
 const ProductCard = (props) => {
   const product = {...props};
-  const dispatch = useDispatch();
-
-  const preventDefault = (event) => {
-    event.preventDefault();
-  };
-  const cartClick = () => {
-    dispatch(addToCart(product));
-  };
-
   return (
         <div className={styles.productCard}>
       <img src={product.image} alt={product.name} className={styles.productImage} />
       <div className={styles.detailsBackground}>
         <h3 className={styles.productName}>{product.name}</h3>
         <hr/>
-        <button className={styles.addToCartButton} onClick={cartClick}>
-            Add to Cart
-        </button>
+        <QuantitySelector product={product}/>
         <p className={styles.productPrice}>${product.price}</p>
       </div>
     </div>
