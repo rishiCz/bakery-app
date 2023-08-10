dff96df25314612407571c16f554aa7ee691f045
Rishi Malik <rishimalik97@gmail.com>
2023-08-10 11:24:16 +0530

    Fixed Route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path = '/' element={<Home/>}/>
-        <Route path = 'cart' element={<Cart/>}/>
+        <Route path = '/cart' element={<Cart/>}/>
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
